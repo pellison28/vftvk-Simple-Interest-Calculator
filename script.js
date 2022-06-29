04841604411c8639a0af8bcde8c5d8222abd48ca
pellison28 <106621531+pellison28@users.noreply.github.com>
2022-06-29 14:09:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+ var principal = document.getElementById("principal").value;
+ var rate = document.getElementById ("rate)").value;
+ var years = document.getElementById ("years").value;
+ var interest = principal * years * rate /100;
+ var year = new Date().getFullYear()+parseInt(years);
+ function updateRate()
+     {
+         var rateval = document.getElementById("rate").value;
+         document.getElementById("rate_val"). innertext=rateval;
+     }
+     document.getElementById("result").innerHTML="If you deposit "+1000000+",\<br\>at an interest rate of "+3.5+"%\<br\>You will receive an amount of "+175000+",\<br\>in the year "+2025+"\<br\>"
